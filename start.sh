c3ef36a3916a09b9dc86f40901f7a5a39e05fc88
Mcl <mixool0204@gmail.com>
2020-11-01 12:43:29 +0800

    rayhero

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# configs
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" >/etc/caddy/Caddyfile
+wget -qO- $CONFIGV2RAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/v2ray.json
+
+# start
+tor &
+
+/v2ray -config /v2ray.json &
+
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
